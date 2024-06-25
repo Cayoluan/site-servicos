209512a6b5563773182e37ad8f914ea5f6ea383e
@Cayoluan <cayoduraes@gmail.com>
2024-06-25 15:23:00 -0300

    Atualizações 25/06/2024

--- scripts.js
+++ scripts.js
@@ -6,20 +6,32 @@ function loadContent(topic) {
             <h1>Como reconectar um Azpost</h1>
             <p>Neste guia, você aprenderá como reconectar um Azpost em poucos passos simples.</p>
             <h2>Passo a Passo</h2>
-            <ol>
-                <li><strong>Verifique as conexões físicas:</strong> Certifique-se de que todos os cabos estão conectados corretamente. Verifique se o dispositivo está ligado.</li>
-                <li><strong>Reinicie o dispositivo:</strong> Desligue o Azpost. Aguarde 10 segundos e ligue novamente.</li>
-                <li><strong>Configuração de Rede:</strong> Acesse as configurações de rede no painel de controle do Azpost. Verifique se os detalhes da rede estão corretos.</li>
-                <li><strong>Teste de Conexão:</strong> Execute um teste de conexão para garantir que o Azpost está online.</li>
+            <ol>              
+                <li>Verifique as conexões no site: https://app.azpost.com.br/conexoes</li>
+                <li>Verifique as conexões inativas que esta em amarelo.</li>
+                <li>Abra o TeamViewer e solicite ao cliente o seu acesso.</li>
+                
             </ol>
-            <h2>Resolução de Problemas Comuns</h2>
+            <h2>Como reconectar?</h2>
             <ul>
-                <li>Se o Azpost não conectar, verifique se há problemas na rede local.</li>
-                <li>Consulte o manual do Azpost para códigos de erro específicos.</li>
+                <li>Clique em conexão</li>
+                 <img class="azpost" src="img/conexao.png" alt="">
+                <li><b>Clique em "Conectar WhatsApp".</b></li>
+                <li>Agora pegue o link da aba que abriu e coloque no navegador do cliente.</li>
+                <li>Feito isso peça para o cliente ler o QRcode na tela e esperar reconectar</li>
+                <li>Vai aparecer uma mensagem de reconectado e pronto já está funcionando perfeitamente.</li>
+               
             </ul>
+            <h2>Possiveis Erros</h2>
+            <ul>
+                <li>WhatsApp do cliente não reconectar: Pedir para o cliente tirar a conexão pelo WhatsApp dele e reconectar novamente.</li>
+                <li>Conexão ficar caindo: Aguardar pelo menos 5 minutos na tela de conectado para estabelecer a conexão</li>
+            </ul>
+
             <h2>Dicas e Melhores Práticas</h2>
             <ul>
-                <li>Mantenha o firmware do Azpost atualizado.</li>
+                <li>Tirar print do cliente reconectado para poder se respaldar e encerrar a solicitação.</li>
+                <img class="azpost" src="img/conectado.png" alt="">
                 <li>Realize manutenções periódicas para evitar problemas de conexão.</li>
             </ul>
             <!-- Conteúdo existente -->
